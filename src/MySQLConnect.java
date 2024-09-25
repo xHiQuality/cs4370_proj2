68542deca2edf44a095ffa883ccb303ca48e6044
Cason Pittman <xhiquality@gmail.com>
2024-09-25 19:29:25 -0400

    todo

--- src/MySQLConnect.java
+++ src/MySQLConnect.java
@@ -2,6 +2,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
+
+//create compile and run scripts and main function
 public class MySQLConnect {
     public static void main(String[] args) {
         String jdbcUrl = "jdbc:mysql://localhost:3306/employees"; // Replace with your DB name
