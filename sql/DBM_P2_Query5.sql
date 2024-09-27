42a3ccd291955bd91587ee1c26cc482bd8b70c7a
Cason Pittman <xhiquality@gmail.com>
2024-09-27 18:43:13 -0400

    updated v2

--- sql/DBM_P2_Query5.sql
+++ sql/DBM_P2_Query5.sql
@@ -1,7 +1,7 @@
 SELECT 
-    E1.emp_no AS emp_no_1, 
+    D1.emp_no AS emp_no_1, 
     D1.dept_no AS department, 
-    E2.emp_no AS emp_no_2
+    D2.emp_no AS emp_no_2
 FROM 
     dept_emp D1
 JOIN 
@@ -10,10 +10,7 @@ JOIN
     AND D1.emp_no <> D2.emp_no 
     AND D1.from_date <= D2.to_date
     AND D1.to_date >= D2.from_date
-JOIN 
-    employees E1 ON D1.emp_no = E1.emp_no
-JOIN 
-    employees E2 ON D2.emp_no = E2.emp_no
 WHERE 
-    E1.emp_no = 10009
-    AND E2.emp_no = 11545;
+    D1.emp_no = 10009
+    AND D2.emp_no = 11545;
+
